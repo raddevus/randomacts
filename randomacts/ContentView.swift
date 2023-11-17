b4efb86ca82f32559991619185197ce2895bb9c3
raddevus <1radeutsch1@gmail.com>
2023-11-17 10:31:09 -0500

    basic setup of initial app

--- randomacts/ContentView.swift
+++ randomacts/ContentView.swift
@@ -9,13 +9,22 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack{
+            Form{
+                Section{
+                    Text("Random Acts of Kindness helps create the opportunity to be more intentional about completing small, positive tasks that benefit others.")
+                }
+                Text("While each individual task might only take a few moments or cost a few dollars, the cumulative impact of your efforts, and the efforts of others using this app, will undoubtedly make the world a more positive place! In addition, research suggests completing tasks like these is likely to strength your connections with others, improve your mood, and possibly your health. If you are ready, then let's get started!")
+                Text(Date.now.formatted())
+            }.toolbar{
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Text("Random Acts of Kindness")
+                        .font(.system(size: 22, weight: .bold))
+                }
+            }.font(.system(size:20))
+                
         }
-        .padding()
+        
     }
 }
 
