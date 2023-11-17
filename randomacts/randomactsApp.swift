b815b46a63531131d8a027ebfc739b9018799b9c
raddevus <1radeutsch1@gmail.com>
2023-11-17 10:09:13 -0500

    Initial Commit

---
+++ randomacts/randomactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  randomactsApp.swift
+//  randomacts
+//
+//  Created by roger deutsch on 11/17/23.
+//
+
+import SwiftUI
+
+@main
+struct randomactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
