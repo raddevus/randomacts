cb2b0e5586bbd3ebe17f9c10aaeaa74f358676dc
raddevus <1radeutsch1@gmail.com>
2024-01-27 12:16:13 -0500

    Cleaned up code & added previewHelper which uses the static var for passing the @State var into the preview -- this is a requirement of the preview system and still causes an error to show up in Xcode (Ambiguous use of 'init(_:traits:body:)') -- weird stuff.

--- randomacts/Views/UserTaskView.swift
+++ randomacts/Views/UserTaskView.swift
@@ -8,29 +8,32 @@
 import SwiftUI
 
 struct UserTaskView: View {
+    
     @Binding var userTask : UserTask?
         
     var body: some View {
-        Text("Screw you, Xcode")
         Group{
             Text("ID: \(userTask?.id ?? 0 )")
-            TextEditor(text: Binding(get: { userTask?.note ?? "" }) {
-                userTask?.note = $0
-                print("userTask.note: \(userTask?.note)")
-            })
-            .padding(.all, 7.0)
-            .opacity(0.80)
-            //.background(Color(red: 0.9, green: 0.9, blue: 0.2))
-            .background(Color.yellow)
+            Group{
+                TextEditor(text: Binding(get: { userTask?.note ?? "" }) {
+                    userTask?.note = $0
+                    print("userTask.note: \(userTask?.note)")
+                })
+                .padding(.all, 7.0)
+                .opacity(0.80)
+                //.background(Color(red: 0.9, green: 0.9, blue: 0.2))
+                .background(Color.yellow)
+            }
             
 //            Text("This is another control")
         }
     }
 }
 
-//#Preview {
-//    @State var noteThing: String? = "this is note"
-//    @State var ut: UserTask? = UserTask()
-//    //UserTaskView(userTask: $ut)//, noteData: $noteThing)
-//    UserTaskView(userTask: $ut, noteData: $noteThing)
-//}
+struct PreviewHelper{
+    @State static var ut1: UserTask? = UserTask() // this is used for the preview
+}
+
+#Preview {
+    UserTaskView(userTask: PreviewHelper.$ut1)
+}
