e8c550caf4d0c53698443699e3973c8325313a06
raddevus <1radeutsch1@gmail.com>
2024-01-28 12:07:09 -0500

    Cleaned up task history layout and now auto-loads the available tasks.

--- randomacts/Views/HistoryView.swift
+++ randomacts/Views/HistoryView.swift
@@ -18,29 +18,6 @@ struct HistoryView: View {
     }
     
     var body: some View {
-        Form{
-            Text("Task History")
-            Section{
-                Text("This will include the list of tasks the user has chosen, associated date they took the task on and a [ ] completed check box to indicate if they completed it")
-                Button("Get Tasks"){
-                    let ut = LocalUserTask(parentView.localUser?.user.id ?? 0)
-                    ut.GetAll(saveUserTasks: saveUserTasks)
-                    //let q = QuoteX()
-                    //q.GetQuote(iso8601Date: "2024-01-01")
-                    //q.Gen1()
-                    print("#########")
-                    //q.Gen2()
-                    // q.Gen3()
-                    //q.GetQuote(iso8601Date: "2023-01-01")
-                }.buttonStyle(.bordered)
-            }
-        }
-        .toolbar{
-            ToolbarItem(placement: .navigationBarLeading) {
-                Text("Task History")
-                    .font(.system(size: 22, weight: .bold))
-            }
-        }
         
         NavigationStack{
             Form{
@@ -72,10 +49,18 @@ struct HistoryView: View {
                     }
                 }
             }.onAppear(perform: {
-                                
+                let ut = LocalUserTask(parentView.localUser?.user.id ?? 0)
+                ut.GetAll(saveUserTasks: saveUserTasks)
+                print("#########")
                 
             })
-                .navigationTitle("KTasks")
+            //.navigationTitle("Task History")
+            .toolbar{
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Text("Task History")
+                        .font(.system(size: 22, weight: .bold))
+                }
+            }
         }
     }
     
