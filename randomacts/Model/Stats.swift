76926381ff1369120a5b395ce63ad9c6ef30dd89
raddevus <1radeutsch1@gmail.com>
2024-02-19 18:04:06 -0500

    Added stats file and Statistics class to begin wrapping the functionality for retrieving the user stats from webApi.

---
+++ randomacts/Model/Stats.swift
@@ -0,0 +1,74 @@
+//
+//  Stats.swift
+//  randomacts
+//
+//  Created by roger deutsch on 2/19/24.
+//
+
+import Foundation
+
+struct UserStats: Codable{
+    let success: Bool
+    let counts: [Int]
+    
+    init(){
+        success = false
+        counts = []
+    }
+}
+
+class Statitiscs {
+    var userStats: UserStats? = nil
+        
+    func GetUserStats(pView: ContentView, saveUserTasks: @escaping (_ pView: ContentView, _ userTasks: [UserTask]) ->(), userId: Int64) -> Bool{
+        let destinationUrl : String = "https://newlibre.com/kind/api/Stats/GetUserStats"
+        
+        guard let url = URL(string: destinationUrl ) else {
+            print("Invalid URL")
+            return false
+        }
+        var request = URLRequest(url: url)
+        
+        request.httpMethod = "POST"
+        let finalData = "userId=\(userId)"
+        request.httpBody = finalData.data(using: String.Encoding.utf8)
+    
+        
+        URLSession.shared.dataTask(with: request) { data, response, error in
+            if let data = data {
+                
+                // print("data: \(data) \(Date())")
+                            do {
+                                let response = try JSONDecoder().decode(UserStats.self, from: data)
+                                print("Decoded UserStats properly.")
+                                self.userStats = response
+                                //print("Success retrieve! \(String(decoding: data, as: UTF8.self))")
+                                print("SAVING SELF!!")
+
+//                                saveUserTasks(pView, self.userStats)
+                                
+                                DispatchQueue.main.async {
+                                    // print ("response: \(data)")
+                                    // print("SUCCESS: \(String(decoding: data, as: UTF8.self))")
+                                    
+                                }
+                                
+                                return
+                                
+                            }catch {
+                                print("\(error)")
+                                print("CATCH: \(String(decoding: data, as: UTF8.self))")
+                            }
+            }
+            else{
+                print("I failed")
+                
+            }
+        }.resume()
+        
+        return true
+    }
+    
+}
+
+
