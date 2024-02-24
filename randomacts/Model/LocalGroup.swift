663cecd253bbcdd16be85daffc278313696e35e4
raddevus <1radeutsch1@gmail.com>
2024-02-24 11:41:10 -0500

    Got the basics set up for the CreateGroup method - work in progress so users can create grups.

--- randomacts/Model/LocalGroup.swift
+++ randomacts/Model/LocalGroup.swift
@@ -39,4 +39,55 @@ struct KGroup: Codable{
             uuid = UUID()
             group = KGroup(uuid.uuidString.lowercased())
         }
+        
+        func CreateGroup(displayUserStats: @escaping (_ userStats: [Int]) ->(), userId: Int64, groupName: String, pwd: String) -> Bool{
+            let destinationUrl : String = "https://newlibre.com/kind/api/Stats/GetUserStats"
+            
+            guard let url = URL(string: destinationUrl ) else {
+                print("Invalid URL")
+                return false
+            }
+            var request = URLRequest(url: url)
+            
+            request.httpMethod = "POST"
+            
+            let finalData = "{\"ownerId\":\(userId),\"memberId\":\(userId),\"guid\":\"\(uuid.uuidString.lowercased())\",\"name\":\"\(groupName)\",\"pwdHash\":\"\(pwd)\",\"created\":null,\"updated\":null,\"active\":true}"
+            request.httpBody = finalData.data(using: String.Encoding.utf8)
+            request.setValue("application/json; charset=utf-8", forHTTPHeaderField: "Content-Type")
+        
+            
+            URLSession.shared.dataTask(with: request) { data, response, error in
+                if let data = data {
+                    
+                    // print("data: \(data) \(Date())")
+                                do {
+                                    let response = try JSONDecoder().decode(KGroup.self, from: data)
+                                    print("Decoded UserStats properly.")
+                                    self.group = response
+                                    //print("Success retrieve! \(String(decoding: data, as: UTF8.self))")
+                                    print("SAVING SELF!!")
+
+//                                    displayUserStats(self.group. ?? [])
+                                    
+                                    DispatchQueue.main.async {
+                                        // print ("response: \(data)")
+                                        // print("SUCCESS: \(String(decoding: data, as: UTF8.self))")
+                                        
+                                    }
+                                    
+                                    return
+                                    
+                                }catch {
+                                    print("\(error)")
+                                    print("CATCH: \(String(decoding: data, as: UTF8.self))")
+                                }
+                }
+                else{
+                    print("I failed")
+                    
+                }
+            }.resume()
+            
+            return true
+        }
     }
